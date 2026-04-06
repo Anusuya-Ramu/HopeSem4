235c2c830f674d7d53d236a6f3cd3b458bbc094e
Anu <anusuyaramu1806@gmail.com>
2026-04-06 22:29:16 +0530

    Day13

--- HolidayTask2/com/CartSystem/util/DiscountUtil.java
+++ HolidayTask2/com/CartSystem/util/DiscountUtil.java
@@ -7,15 +7,15 @@ public class DiscountUtil {
 
         if (totalAmount >= 5000) {
             System.out.println("20% Discount Applied!");
-            return totalAmount * 0.8; // 20% off
+            return totalAmount * 0.8; 
         } 
         else if (totalAmount >= 2000) {
             System.out.println("10% Discount Applied!");
-            return totalAmount * 0.9; // 10% off
+            return totalAmount * 0.9;
         } 
         else if (totalAmount >= 1000) {
             System.out.println("5% Discount Applied!");
-            return totalAmount * 0.95; // 5% off
+            return totalAmount * 0.95; 
         }
 
         System.out.println("No discount applied.");
